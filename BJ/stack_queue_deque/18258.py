64565372b63f1db278e9b201a22bf4b3dd68e0ea
ksuncho <momo.cho@gmail.com>
2024-06-19 00:17:54 +0900

    add:queue

--- BJ/stack_queue_deque/18258.py
+++ BJ/stack_queue_deque/18258.py
@@ -1,13 +1,33 @@
 from collections import deque
-N = int(input())
+import sys
+N = int(sys.stdin.readline().rstrip())
 queue = deque()
 for _ in range(N):
-    command, input = input().strip()
-    input = int(input)
-    if command == 'push':
-        queue.append(input)
-    if command == 'front':
-        print(queue[0])
-    if command == 'back':
-        print(queue[-1])
-    if 
+    command = [x for x in sys.stdin.readline().split()]
+    if (len(command) == 2):
+        x = int(command[1])
+    if command[0] == 'push':
+        queue.append(x)
+    elif command[0] == 'front':
+        if (len(queue)==0):
+            print(-1)
+        else:
+            print(queue[0])
+    elif command[0] == 'back':
+        if (len(queue)==0):
+            print(-1)
+        else:
+            print(queue[-1])
+    elif command[0] == 'size':
+        print(len(queue))
+    elif command[0] == 'empty':
+        if len(queue)==0:   
+            print(1)
+        else:
+            print(0)
+    elif command[0] == 'pop':
+        if (len(queue)==0):
+            print(-1)
+        else:
+            print(queue.popleft())
+    
