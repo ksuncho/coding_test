c4120fb32377bbd14142b6e97f346394d9d8c701
ksuncho <momo.cho@gmail.com>
2024-08-20 23:50:11 +0900

    fix: update 13460.py

--- BJ/Samsung_SW/13460.py
+++ BJ/Samsung_SW/13460.py
@@ -1,4 +1,56 @@
 from collections import deque
+table = [['#','#','#','#','#'],
+         ['#','.','.','B','#'],
+         ['#','.','#','.','#'],
+         ['#','R','O','.','#'],
+         ['#','#','#','#','#']]
+#d = [ l ,u, r, d]
+dx = [-1, 0, 1, 0]
+dy = [0, -1, 0, 1]
+def bfs(rx,ry,bx,by,tx,ty):
+    queue = deque([rx,ry,bx,by,0])
+    while queue:
+        rxx, ryy, bxx, byy, d = queue.popleft()
+        while True:
+            nrx = rxx + dx[d]
+            nry = ryy + dy[d]
+            nbx = bxx + dx[d]
+            nby = byy + dy[d]
+            if ((nrx == nbx) and (nry == nby)) \
+                or ((table[nry][nrx] == '#') and (table[nby][nbx] == '#')):
+                break
+            if table[nry][nrx] == '#':
+                bxx = nbx
+                byy = nby
+                continue
+            if table[nby][nbx] == '#':
+                rxx = nrx
+                ryy = nry
+                continue
+            bxx = nbx
+            byy = nby                
+            rxx = nrx
+            ryy = nry
+        if (rxx == tx) and (ryy == ty):
+            break
+        for i in range(4):
+            queue.append([rxx,ryy,bxx,byy,i])
+        cnt += 1
+    return cnt
 
-def bfs(rx,ry,bx,by):
-     
+tx, ty, rx, ry, bx, by = 0, 0, 0, 0, 0, 0
+for i in range(5):
+    for j in range(5):
+        if table[i][j] == 'O':
+            tx = j
+            ty = i            
+        if table[i][j] == 'R':
+            rx = j
+            ry = i
+        if table[i][j] == 'B':
+            bx = j
+            by = i
+
+print(bfs(rx,ry,bx,by,tx,ty))
+
+        
