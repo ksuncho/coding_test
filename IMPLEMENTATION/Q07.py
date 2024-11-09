2e851646e6f18b73b53626c87696c5c8e7016d80
ksuncho <momo.cho@gmail.com>
2024-11-09 09:42:06 +0900

    1108

---
+++ IMPLEMENTATION/Q07.py
@@ -0,0 +1,8 @@
+N = str(input())
+data = [int(i) for i in N]
+length = len(data)
+print(length)
+if sum(data[0:length//2])==sum(data[length//2:length]):
+    print('LUCKY')
+else:
+    print('READY')
