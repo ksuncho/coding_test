9b964ff1c508af74338d573065fcd280d512b982
ksuncho <momo.cho@gmail.com>
2024-11-11 01:22:54 +0900

    1111_1

--- IMPLEMENTATION/Q12.py
+++ IMPLEMENTATION/Q12.py
@@ -5,32 +5,30 @@ for _ in range(N):
     build_frame.append(list(map(int,input().split(','))))
 print(build_frame)
 
-def possible(frame,result):
-    x, y, t, op = frame 
-    if op==1:   
+def possible(result):
+    for frame in result:
+        x, y, t = frame
         if t==0:
-            if(y==0 or [x,y-1,0] in result) or ([x-1,y,1] in result):
-                return True
+            if y==0 or ([x,y-1,0] in result) or ([x-1,y,1] in result) or ([x,y,1] in result):
+                continue
             return False
-        if t==1:
-            if(y!=0) :
-                if([x,y-1,0] in result) or ([x-1,y,1] in result) or ([x+1,y-1,0] in result):
-                    return True
+        elif t==1:
+            if([x,y-1,0] in result)  or ([x+1,y-1,0] in result) or (([x-1,y,1] in result) and ([x+1,y,1] in result)):
+                continue
             return False
-    else:
-        if ([x+1,y-1,0] in result) or ([x-1,y+1,1] in result):
-            return True
-        return False
+    return True
 
 result=[]
 for i in build_frame:
     x, y, t, op = i
     if op==1:
-        if possible(i,result):
-            result.append([x,y,t])
-    elif op==0:
-        if possible(i,result):
-            result.remove([x,y,t])
-    print(result)
+        result.append([x, y, t])
+        if not possible(result):
+            result.remove([x, y, t])
+    if op==0:
+        result.remove([x, y, t])
+        if not possible(result):
+            result.append([x, y, t])
+    #print(result)
 result.sort()
 print(result)
