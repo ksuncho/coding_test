5ffaafb12c937c53ddf980f186986ce795efb5d1
ksuncho <momo.cho@gmail.com>
2024-11-24 22:52:46 +0900

    1124

--- DFS_BFS/Q15.py
+++ DFS_BFS/Q15.py
@@ -1,44 +1,35 @@
+#https://www.acmicpc.net/problem/18352
 from collections import deque
-from itertools import permutations
 n, m , k , x = map(int,input().split())
-array = [[] for i in range(n)]
-#print(array)
+array = [[] for i in range(n+1)]
 
-cities = set()
 for _ in range(m):
     i,j=map(int,input().split())    
-    array[i-1].append(j)    
-    cities = cities | set((i,j))    
-#print(array)
+    array[i].append(j)    
 
-
-def bfs (array,start,end,visited):
-    queue=deque()
-    queue.append(start)
-    visited[start-1]=1
-    cnt=0       
+def bfs (array,start,visited):
+    queue=deque([start])
     while queue:
         v = queue.popleft()
-        if v == end:
-            break
-        for i in array[v-1]:
-            if not visited[i-1]:
+        for i in array[v]:
+            if visited[i] == -1:
                 queue.append(i)
-                visited[i-1]=1
-                cnt+=1
-                print(i,queue)     
-    return cnt
+                visited[i]=visited[v]+1
+                #print(i,queue)     
+    #return visited
+    return
 
-combi = permutations(cities,2)
-for start,end in list(combi):    
-    if start == x:
-        print(start,end) 
-        visited = [0]*len(cities)
-        count = bfs(array,start,end,visited)
-        #count = sum(visited)
-        print(f'{start} {end} {visited} {count}')
-        if count==k+1:
-            print(f'find:{start} {end}!!')
+distance = [-1]*(n+1)
+distance[x]=0
+bfs(array,x,distance)
+check = False
+for end in range(1,n+1):    
+    if distance[end]==k:
+        print(f'{end}')
+        check = True
+
+if check is False:
+    print(-1)
         
     
 
