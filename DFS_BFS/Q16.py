047f42e44eae49b619b982ac6b069e756b495a59
ksuncho <momo.cho@gmail.com>
2024-11-28 23:41:34 +0900

    1128

--- DFS_BFS/Q16.py
+++ DFS_BFS/Q16.py
@@ -9,20 +9,16 @@ maps = []
 for i in range(n):
     maps.append(list(map(int,input().split())))
     
-dx = [1, 0, -1, 0]
-dy = [0, 1, 0, -1]
-
 def dfs(x,y):
     #print(x,y)
     if (x < 0 or x >=n) or (y <0 or y >=m):
         return False
     if maps[x][y]==0:
         maps[x][y]=2  
-        for i in range(4):            
-            nx = x+dx[i]
-            ny = y+dy[i]
-            #print(f'nx={nx},ny={ny}')
-            dfs(nx,ny)
+        dfs(x+1,y)
+        dfs(x,y+1)
+        dfs(x-1,y)
+        dfs(x,y-1)        
         return True
     return False
 
@@ -33,7 +29,8 @@ for i in range(n):
             zeros.append((i,j))
 appended_wall = combinations(zeros,3)
 #print(list(appended_wall))
-ans = 0
+ans = m*n
+
 for x,y,z in appended_wall:
     #print(f'x={x}, y={y}, z={z}')
     maps[x[0]][x[1]]=1
@@ -44,10 +41,11 @@ for x,y,z in appended_wall:
     result = 0
     for i in range(n):
         for j in range(m):
-            if dfs(i,j)==False:
+            if dfs(i,j)==True:
                 result+=1
                 #print(dfs(i,j))
-    ans = max(ans,result)
+    ans = min(ans,result)
+    print(f'x={x}, y={y}, z={z}, ans={ans}')
     maps[x[0]][x[1]]=0
     maps[y[0]][y[1]]=0
     maps[z[0]][z[1]]=0
@@ -59,6 +57,6 @@ for x,y,z in appended_wall:
 #             print(dfs(i,j))    
 
 
-# for i in range(len(maps)):
-#     print(maps[i])
+for i in range(len(maps)):
+    print(maps[i])
 print(f'answer={ans}')
