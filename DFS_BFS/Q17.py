3a416c28e9694f8ce548d557c4db9ba6bdc74c28
ksuncho <momo.cho@gmail.com>
2024-12-03 23:18:52 +0900

    1203

--- DFS_BFS/Q17.py
+++ DFS_BFS/Q17.py
@@ -11,15 +11,15 @@ def dfs(x,y,v,r):
     if r==0:
         return False
     aa=array[x][y]
-    print(f'x={x},y={y},v={v},r={r},array[x][y]={aa}')
+    #print(f'x={x},y={y},v={v},r={r},array[x][y]={aa}')
     if array[x][y]==0:
         array[x][y]=v
         dfs(x-1,y,v,r-1)
         dfs(x,y-1,v,r-1)
         dfs(x+1,y,v,r-1)
         dfs(x,y+1,v,r-1)
-        for i in range(n):
-            print(array[i])
+        # for i in range(n):
+        #     print(array[i])
         return True
     return True
 virus = []
@@ -27,13 +27,15 @@ for i in range(n):
     for j in range(n):
         if array[i][j]!=0:
             virus.append((i,j,array[i][j]))
-for minute in range(s,0,-1):
-    for i in virus:
-        dfs(i[0],i[1]+1,i[2],1)
-        dfs(i[0],i[1]-1,i[2],1)
-        dfs(i[0]+1,i[1],i[2],1)
-        dfs(i[0],i[1]+1,i[2],1)
-        print(i)
+#for minute in range(s,0,-1):
+for i in virus:
+    dfs(i[0],i[1]+1,i[2],s)
+    dfs(i[0],i[1]-1,i[2],s)
+    dfs(i[0]+1,i[1],i[2],s)
+    dfs(i[0],i[1]+1,i[2],s)
+    #print(i)
 
-for i in range(n):
-    print(array[i])
+# for i in range(n):
+#     print(array[i])
+
+print(array[x-1][y-1])
