2ebb2d18d149c7922076aadbb53a69e8faab8d3b
ksuncho <momo.cho@gmail.com>
2025-01-15 23:58:30 +0900

    0105

--- EDUPRO/12.py
+++ EDUPRO/12.py
@@ -1,5 +1,6 @@
 import sys
 import string
+from collections import defaultdict
 
 if sys.platform == 'win32':
     sys.stdin = open('C:/Users/User/momo/python/coding_test/EDUPRO/12.txt')
@@ -14,21 +15,45 @@ if len(SA) > len(SB):
     TAR = SA
     SOR = SB
 DB = dict()
-cnt = dict()
+cnts = dict()
+cntt = dict()
 
-for end in range(len(SOR),0,-1):
-    for start in range(len(SOR)):
-        if start > end:
+for word in string.ascii_lowercase:
+    cntt[word]=0
+#for k in range(len(SOR),-1,-1):
+#cnt = [0]*26
+for k in range(2,1,-1):
+    print(k)
+    for word in string.ascii_lowercase:
+        cnts[word]=0
+    for i in range(len(SOR)):
+        if len(SOR)-k-i < 0:
             break
-        template = SOR[start:end]
-        for word in string.ascii_lowercase:
-            cnt[word]=0
+        template = SOR[i:k+i]        
         for word in template:
-            cnt[word] += 1
-        freq = tuple(cnt.values())
-        DB[freq] = 1
+            cnts[word] += 1
+        freqs = tuple(cnts.values())
+        if freqs not in DB:
+            DB[freqs] = 1        
+    print(cnts)
 
-    
+    flag = 0
+    for word in string.ascii_lowercase:
+        cntt[word]=0
+    for j in range(len(TAR)):
+        if len(TAR)-k-j < 0:
+            break
+        key = TAR[j:k+j]        
+        for word in key:
+            cntt[word] += 1
+        freqt = tuple(cntt.values())
+        if freqt in DB:
+            flag = 1
+            print(f'{k} break!!!!')
+            break            
+    if flag:
+         break      
 
-print(SOR)
-print(cnt.values())
+#print(SOR)
+#print(k)
+#print(DB)
