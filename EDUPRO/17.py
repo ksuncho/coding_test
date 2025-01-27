9465aa83130ec0eb46b72f534c0c15a1866220a5
ksuncho <momo.cho@gmail.com>
2025-01-28 00:16:32 +0900

    0128

--- EDUPRO/17.py
+++ EDUPRO/17.py
@@ -6,16 +6,24 @@ from heapq import heappop, heappush
 input = sys.stdin.readline
 
 def eval(sid,pid,score):
-    global scorelist
+    global scorelist, avgval, sumval
     sid = int(sid)
     pid = int(pid)
     score = int(score)    
     scorelist[sid]=[pid,score]
-    avgval[pid] += score/len(scorelist[sid])
-    sumval[pid] += score
+    #print(pid,len(scorelist[sid]))
+    #avgval[pid] = 0
+    #sumval[pid] += score
+    for id in scorelist:
+        x, y = scorelist[id]
+        sumval[x] += y
+        #avgval[i[0]] += 1
+        #print(i)
+    print(pid, avgval[pid])
     pass
 
 def clear(sid):
+    removelist.append(sid)
     pass
 
 def update(sid,pid,score):
@@ -25,8 +33,9 @@ def update(sid,pid,score):
 def sumq(flag):
     sumpq = []
     avgpq = []
+
     if int(flag) == 1: 
-        for sid in range(scorelist):            
+        for sid in scorelist:            
             heappush(sumpq, (scorelist[sid][1],scorelist[sid][0]))
     pass
 
@@ -36,9 +45,9 @@ def avgq(flag):
 n, m = map(int, input().split())
 q = int(input())
 scorelist = defaultdict(list)
-sumval = [0]*m
-avgval = [0]*m
-
+sumval = [[0] for _ in range(m+1)]
+avgval = [[0] for _ in range(m+1)]
+removelist = []
 for _ in range(q):
     cmd, *val = input().split()
     if cmd == 'EVAL': eval(*val)
