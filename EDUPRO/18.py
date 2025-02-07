92f52a65be19f1f33cfb4d28fa8245566e778643
ksuncho <momo.cho@gmail.com>
2025-02-08 00:26:02 +0900

    0208

--- EDUPRO/18.py
+++ EDUPRO/18.py
@@ -3,3 +3,15 @@ if sys.platform == 'win32':
     sys.stdin = open('C:/Users/User/momo/python/coding_test/EDUPRO/18.txt')
 
 input = sys.stdin.readline
+from collections import defaultdict
+
+devlist = defaultdict(list)
+
+def register(pid, salary, C, J, P):
+    devlist[pid]=[salary,C,J,P]
+
+def cancel(pid):
+    devlist[pid].clear()
+
+def update(pid,flag,X):
+    devlist[pid][flag+1]=X
